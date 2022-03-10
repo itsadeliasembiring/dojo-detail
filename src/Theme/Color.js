4d9b0a0cf63b891a46011a9449d7169adada8ad6
Adelia <adeliasmilecontact@gmail.com>
2022-03-10 09:38:04 +0700

    Dojo Profile

---
+++ src/Theme/Color.js
@@ -0,0 +1,12 @@
+const Colors = {
+  black: "#000000",
+  white: "#ffffff",
+  darkGray: "#979595",
+  gray: "#969696",
+  lightGray: "#F4F4F4",
+  yellow: "#FFB800",
+  orange: "#F78104",
+  purple: "#7349AC",
+};
+
+export default Colors;
